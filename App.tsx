dedf427204d147167a3ee428171b21989c91ac25
PamelaMBadermann <badermann.pamela@gmail.com>
2026-06-29 20:44:35 -0300

    refactor: inicializa banco SQLite na inicialização da aplicação

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import Navigation from './src/navigation';
+import { criarBanco } from './src/database/migrations';
 
 export default function App() {
+  useEffect(() => {
+    criarBanco();
+  }, []);
+  
   return (
     <SafeAreaProvider>
       <Navigation />
